4c475d3bfef7c242c100a15dea173940a7c7ddce
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-19 19:02:07 +1100

    Enhance game setup with age-based play eligibility and conditional gameplay access

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ print('Welcome to my first game!')
 name = input("What is your name? ").lower()
 age = int(input("What is your age? ")).lower()
 
+print("Hello", name, "you are", age, "years old")
+
+if age >= 18:
+    print("You are old enough to play!")
+    wants_to_play = input("Do you want to play? ").lower()
+    if wants_to_play == "yes":
+        print("Let's play!")
+elif age >= 14:
+    print("You can play with supervision.")
+    supervision = input("Are you playing under supervision? ").lower()
+    if supervision == "yes":
+        print("Let's play")
+    elif supervision == "no":
+        print("Come back with a supervision")
+else:
+    print("You are not old enough to play.....")
